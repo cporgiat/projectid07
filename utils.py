ac16bd9838b8c0cb7b4a340c75204fc6c44b64df
Harris <cporgiatzidis@outlook.com>
2024-05-08 18:19:01 +0300

    customers+appointments basic functions - add utils module for helper functions (eg. validate input) - menus decoupled from classes - Fixed customer fullname on every appointment maintenance - Display customer fullname - Refactored to separate modules - Refactored to remove global variables - change appointment not working yet - partial ref. integrity

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 def validate_input_only_letters(ap_text):
     if(re.match(r'^[A-Za-zΑ-Ωα-ω]+$', ap_text)):
@@ -17,6 +18,14 @@ def validate_input_email(ap_text):
         return True
     else:
         return False
+
+def validate_input_datetime(ap_text):
+    try:
+        datetime.strptime(ap_text, "%Y-%m-%d %H:%M")
+        return True
+    except ValueError:
+        return False
+
 
 def get_name(ap_text):
 
@@ -48,5 +57,27 @@ def get_email(ap_text):
             break
         else:
             print("Μη σωστο email. Παρακαλω χρησιμοποιηστε μονο πεζους Λατινικους χαρακτηρες χωρις κενα. Δοκιμαστε παλι.")
+
+    return ap_input
+
+def get_datetime(ap_text):
+
+    while(True):
+        ap_input=input(ap_text)
+        if(validate_input_datetime(ap_input)):
+            break
+        else:
+            print("Μη σωστη ημερομηνια. Παρακαλω εισαγετε ημερομηνια στην μορφη ΥΥΥΥ-ΜΜ-DD HH:MI. Δοκιμαστε παλι.")
+
+    return ap_input
+
+def get_number(ap_text):
+
+    while(True):
+        ap_input=input(ap_text)
+        if(validate_input_only_numbers(ap_input)):
+            break
+        else:
+            print("Παρακαλω χρησιμοποιηστε μονο αριθμους χωρις κενα. Δοκιμαστε παλι.")
 
     return ap_input
