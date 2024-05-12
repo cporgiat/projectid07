2f35721ecc6278883bb8b96cd195b858ad456215
VasilisTsoutsas <tsoutsasv@gmail.com>
2024-05-12 23:25:57 +0300

    New

--- search_menu.py
+++ search_menu.py
@@ -1,6 +1,6 @@
 from search import (
     search_by_date,
-    search_by_customer_name,
+    search_by_customer_email,
     print_results,
     export_to_excel
 )
@@ -10,7 +10,7 @@ def search_menu():
     while True:
         print("\n--- Μενού Αναζήτησης ---")
         print("1. Αναζήτηση με βάση την ημερομηνία")
-        print("2. Αναζήτηση με βάση το όνομα του πελάτη")
+        print("2. Αναζήτηση με βάση το E-mail")
         print("99. Επιστροφη στο προηγουμενο Menu")
 
         choice = input("Επιλέξτε μια επιλογή: ")
@@ -21,22 +21,21 @@ def search_menu():
             results = search_by_date(date)
             if results:
                 print_results(results)  # Εκτύπωση των αποτελεσμάτων
-                export_choice = input("Θέλετε να εξαγάγετε σε Excel; (ναι/όχι): ")
-                if export_choice.lower() == "ναι":
+                export_choice = input("Θέλετε να εξαγάγετε σε Excel; (1.Ναι / 2.Οχι): ")
+                if export_choice.lower() == "1":
                     filename = input("Πληκτρολογήστε το όνομα του αρχείου Excel (χωρίς κατάληξη): ")
                     export_to_excel(results, filename + ".xlsx")
             else:
                 print("Δεν βρέθηκαν αποτελέσματα.")
 
         elif choice == "2":
-            # Αναζήτηση με βάση το όνομα του πελάτη
-            firstname = input("Εισάγετε το όνομα του πελάτη: ")
-            lastname = input("Εισάγετε το επώνυμο του πελάτη: ")
-            results = search_by_customer_name(firstname, lastname)
+            # Αναζήτηση με βάση το email του πελάτη
+            email = input("Εισάγετε το email του πελάτη: ")
+            results = search_by_customer_email(email)
             if results:
                 print_results(results)  # Εκτύπωση των αποτελεσμάτων
-                export_choice = input("Θέλετε να εξαγάγετε σε Excel; (ναι/όχι): ")
-                if export_choice.lower() == "ναι":
+                export_choice = input("Θέλετε να εξαγάγετε σε Excel; (1.Ναι / 2.Οχι): ")
+                if export_choice.lower() == "1":
                     filename = input("Πληκτρολογήστε το όνομα του αρχείου Excel (χωρίς κατάληξη): ")
                     export_to_excel(results, filename + ".xlsx")
             else:
